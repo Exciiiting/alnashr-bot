a9abe47d578ee1dce774250c0256b8e399ebe846
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-12-03 16:52:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,16 +2,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 let timer;
- 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log("Savage");
-});
-
 
 let d5loa = [];
 
-console.log("BOT ONLINE");
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Omar.");
+});
+
 
 client.on("guildMemberAdd", member => {
 
@@ -23,29 +21,29 @@ client.on("guildMemberAdd", member => {
 
 
     timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [`
+  let words = [`**
   أيُها المُنهك,قد تكُون الحياة غير مُنصِفة مَعك 
 وفي كُل مرة تُحاول النهُوض فيها تُسقِطكَ أرضاً 
 لا تجزع وكُن قوياً إجمع شُتاتك وقِف مُستقيماً إصبر 
 قد يأتي مساءً يمحِي وجع الأمس وفرحاً يُنسيك مُر الأيام 
 طابَ مسائك وطابت أوجاعُ  | تنورنا يبعدي قلبي.  
-*Welcome to* **Spirit.**
-   https://discord.gg/VFStUas`,`
+*Welcome to* **Fast**
+   https://discord.gg/EGn6JAg **`,`**
   ي جميل , تعال ابغى اسممع صوتك .:candle::black_heart:.
-  رابط:https://discord.gg/VFStUas 
--  مابقول طبح بحضني , بس تعال وارضيك ياعين ابوي .  ؟ :heartbeat:.
+  رابط:https://discord.gg/EGn6JAg
+-  مابقول طب بحضني , بس تعال وارضيك ياعين ابوي .  ؟ :heartbeat:.
 
-  LINK : https://discord.gg/VFStUas`,`
+  LINK : https://discord.gg/EGn6JAg ** `,`**
     :hearts:️:paperclips:.
   :heart_eyes: :heart: 
   م أبغى ابثرك لكن خلك لطيف وامش ياخي . ؟  :musical_score::sparkles:
-                                 [ https://discord.gg/VFStUas ] 
-  وههذي لك ي جمميل ؟ ...`]
+                                 [ https://discord.gg/EGn6JAg ] 
+  وههذي لك ي جمميل ؟ ...**`,`** أسعد الله اوقاتك بكل خير .. حياك الله بسيرفرنا .. :sunflower: ** https://discord.gg/EGn6JAg `]
   setTimeout(() =>{
   member.createDM().then(function (channel) {
   return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
 }, timer)
-10000})
+})
     
     client.login(process.env.BOT_TOKEN);
